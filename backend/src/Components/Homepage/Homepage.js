407bd8432e523b485eeee19e5463194fa32299c2
Vardaan07 <vardaansrivastava2215@gmail.com>
2023-06-19 02:15:42 +0530

    second commit

---
+++ backend/src/Components/Homepage/Homepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export const Homepage = ({ setLoginUser }) => {
+    return ( <
+        >
+        <
+        div >
+        <
+        h1 > This is the HomePage < /h1> <
+        /div> <
+        Button onClick = {
+            () => setLoginUser({}) } > Logout < /Button> <
+        />
+    );
+};
